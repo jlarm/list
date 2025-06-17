2946d14b6442725eec2f4641c6664357d87d8b91
jlarm <117611704+jlarm@users.noreply.github.com>
2025-06-17 16:02:44 -0500

    feat:added item model, migration and observer

---
+++ app/Models/Item.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+#[ObservedBy(Item::class)]
+final class Item extends Model
+{
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'purchased' => 'boolean',
+        ];
+    }
+}
